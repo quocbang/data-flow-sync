4215c50b8e6230c60b65df1ebe727a929c3fff63
quocbang <bangquoc9@gmail.com>
2023-08-15 10:40:52 +0700

    fix: a function has multiple roles

--- server/utils/roles/role.go
+++ server/utils/roles/role.go
@@ -4,17 +4,21 @@ import (
 	"github.com/quocbang/data-flow-sync/server/utils/function"
 )
 
-var funcPermission map[function.FuncName]Roles
+var funcPermission map[function.FuncName][]Roles
 
 func HasPermission(f function.FuncName, role Roles) bool {
-	if r, ok := funcPermission[f]; ok {
-		return r == role
+	if rs, ok := funcPermission[f]; ok {
+		for _, r := range rs {
+			return r == role
+		}
 	}
 	return false
 }
 
 func init() {
-	funcPermission = map[function.FuncName]Roles{
-		function.FuncName_UPLOAD_LIMITARY_HOUR: Roles_LEADER,
+	funcPermission = map[function.FuncName][]Roles{
+		function.FuncName_UPLOAD_LIMITARY_HOUR: {
+			Roles_LEADER,
+		},
 	}
 }
