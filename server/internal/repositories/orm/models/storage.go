45175a7a44ebf938b957392e643b1b7c1faf52a7
caosown <sonchu8699@gmail.com>
2023-08-26 15:15:56 +0700

    fix: data storage table

--- server/internal/repositories/orm/models/storage.go
+++ server/internal/repositories/orm/models/storage.go
@@ -5,13 +5,13 @@ import (
 )
 
 type DataStorage struct {
-	ID        string     `gorm:"type:text,primaryKey"`
-	Type      type_.Type `gorm:"not null"`
-	Content   []byte     `gorm:"type:bytea; not null"`
-	CreatedBy string     `gorm:"type:text, not null"`
+	ID        string     `gorm:"column:id;type:text;primaryKey"`
+	Type      type_.Type `gorm:"type:int;not null"`
+	Content   []byte     `gorm:"type:bytea;not null"`
+	CreatedBy string     `gorm:"type:text;not null"`
 	CreatedAt int64      `gorm:"type:bigint;autoCreateTime"`
 }
 
-func (DataStorage) Table() string {
+func (d *DataStorage) TableName() string {
 	return "data_storage"
 }
