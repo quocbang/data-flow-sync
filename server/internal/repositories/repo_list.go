c122904463faab2c94e6036d6715e22779ed0fae
caosown <sonchu8699@gmail.com>
2023-08-28 09:43:23 +0700

    fix: upsertStation

--- server/internal/repositories/repo_list.go
+++ server/internal/repositories/repo_list.go
@@ -3,8 +3,10 @@ package repositories
 import (
 	"context"
 
+	"github.com/go-openapi/runtime/middleware"
 	"github.com/quocbang/data-flow-sync/server/internal/repositories/orm/models"
-	model "github.com/quocbang/data-flow-sync/server/swagger/models"
+	mod "github.com/quocbang/data-flow-sync/server/swagger/models"
+	"github.com/quocbang/data-flow-sync/server/swagger/restapi/operations/station"
 )
 
 type Services interface {
@@ -14,7 +16,7 @@ type Services interface {
 }
 
 type StationServices interface {
-	UpsertStationDataStorage(context.Context, *model.CreateStationDataStorage) (CreateStationDataStorageReply, error)
+	UpsertStationDataStorage(params station.CreateStationDataStorageParams, principal *mod.Principal) middleware.Responder
 }
 
 type StationGroupServices interface {
--- server/internal/repositories/services/station/impl.go
+++ server/internal/repositories/services/station/impl.go
@@ -1,17 +1,19 @@
 package station
 
 import (
-	"context"
 	"fmt"
+	"net/http"
 
+	"github.com/go-openapi/runtime/middleware"
 	"github.com/go-redis/redis/v9"
 	"gorm.io/gorm"
 	"sigs.k8s.io/yaml"
 
 	type_ "github.com/quocbang/data-flow-sync/server/assets/protobuf/types"
 	"github.com/quocbang/data-flow-sync/server/internal/repositories"
-	station "github.com/quocbang/data-flow-sync/server/internal/repositories/orm/models"
+	repo "github.com/quocbang/data-flow-sync/server/internal/repositories/orm/models"
 	"github.com/quocbang/data-flow-sync/server/swagger/models"
+	"github.com/quocbang/data-flow-sync/server/swagger/restapi/operations/station"
 )
 
 type service struct {
@@ -26,23 +28,33 @@ func NewService(pg *gorm.DB, redis *redis.Client) repositories.StationServices {
 	}
 }
 
-func (s service) UpsertStationDataStorage(ctx context.Context, req *models.CreateStationDataStorage) (repositories.CreateStationDataStorageReply, error) {
+func (s service) UpsertStationDataStorage(params station.CreateStationDataStorageParams, principal *models.Principal) middleware.Responder {
 	errorMessage := ""
-	data, err := yaml.YAMLToJSON([]byte(req.Content))
+	data, err := yaml.YAMLToJSON([]byte(params.Body.Content))
 	if err != nil {
-		errorMessage += fmt.Sprintf("%s station is not a valid YAML: %v\n", req.ID, err)
+		errorMessage += fmt.Sprintf("%s station is not a valid YAML: %v\n", params.Body.ID, err)
 	}
 
 	if errorMessage != "" {
-		return repositories.CreateStationDataStorageReply{}, fmt.Errorf(errorMessage)
+		// return repositories.CreateStationDataStorageReply{}, fmt.Errorf(errorMessage)
+		return station.NewCreateStationDataStorageDefault(http.StatusBadRequest).WithPayload(&station.CreateStationDataStorageDefaultBody{
+			Details: errorMessage,
+		})
 	}
 
-	reply := s.pg.Create(&station.DataStorage{
-		ID:        req.ID,
+	reply := s.pg.Create(&repo.DataStorage{
+		ID:        params.Body.ID,
 		Type:      type_.Type_STATION,
 		Content:   data,
 		CreatedBy: "tester_AI",
 		CreatedAt: 454545,
 	})
-	return repositories.CreateStationDataStorageReply{RowsAffected: reply.RowsAffected}, reply.Error
+
+	if reply.Error != nil {
+		return station.NewCreateStationDataStorageDefault(http.StatusBadRequest).WithPayload(&station.CreateStationDataStorageDefaultBody{
+			Details: reply.Error.Error(),
+		})
+	}
+
+	return station.NewCreateStationDataStorageOK()
 }
